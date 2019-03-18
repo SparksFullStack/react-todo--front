53cc9ee77966c48fdc1425aba701ce8f21d58e51
SparksFullStack <csparks@antioch.edu>
2019-03-18 14:26:11 -0700

    added methods to update the local state

--- src/Components/ForgotPass.js
+++ src/Components/ForgotPass.js
@@ -20,21 +20,27 @@ import {
 
 class ForgotPass extends Component {
     state = {
-        answerStatus: false
+        answerStatus: false,
+        securityQuestionAnswer: "",
     }
-
 
     handleRenderFormType = () => {
         if (!this.state.answerStatus) {
             return (
                 <Fragment>
-                    <ModalHeader className="text-center" toggle={this.props.toggle}>Please answer the security question</ModalHeader>
+                    <ModalHeader className="text-center" toggle={this.props.toggle}>Please answer the security questions</ModalHeader>
                     <ModalBody>
                         <Form>
                             <FormGroup row>
-                                <Label className="forgotPass--labels" for="forgotPass--security" sm={12}>What was your mother's maiden name?</Label>
+                                <Label className="forgotPass--labels" for="forgotPassEmail" sm={12}>What's your email address?</Label>
                                 <Col sm={12}>
-                                    <Input  type="email" name="signinEmail" id="signin-email" placeholder="enter your answer..." bsSize="md" />
+                                    <Input value={this.state.securityQuestionAnswer} onChange={this.handleUpdateForm('securityQuestionAnswer')} type="email" name="forgotPassEmail" id="forgotPassEmail" placeholder="enter your answer..." bsSize="md" />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label className="forgotPass--labels" for="forgotPassSecurity" sm={12}>What was your mother's maiden name?</Label>
+                                <Col sm={12}>
+                                    <Input value={this.state.securityQuestionAnswer} onChange={this.handleUpdateForm('securityQuestionAnswer')} type="text" name="forgotPasssecurity" id="forgotPassSecurity" placeholder="enter your answer..." bsSize="md" />
                                 </Col>
                             </FormGroup>
                         </Form>
@@ -47,6 +53,14 @@ class ForgotPass extends Component {
         } else return <h1>idk bro</h1>
     }
 
+    handleUpdateForm = (formType) => event => {
+        this.setState({ [formType]: event.target.value });
+        console.log(this.state[formType]);
+    }
+
+    handleSubmitAnswer = () => {
+        axios.post('http://localhost:3001/auth/forgot_pass', {})
+    }
 
     render() {
         return (
